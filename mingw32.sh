197907bb5546872231c02f6625266623f256c762
EBK21 <chkd13303@gmail.com>
2024-04-09 11:52:26 +0800

    update crt version

--- mingw32.sh
+++ mingw32.sh
@@ -20,7 +20,7 @@ export AR="gcc-ar"
 export RANLIB="gcc-ranlib"
 export NM="gcc-nm"
 
-curl -L "https://github.com/eebssk1/mingw-crt-build/releases/download/677b4e7b/mingw-crt.tgz" | tar -zxf -
+curl -L "https://github.com/eebssk1/mingw-crt-build/releases/download/4b06d704/mingw-crt.tgz" | tar -zxf -
 
 echo current utc time 1 is $(date -u)
 
