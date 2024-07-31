3782cad0e62fe8027e309bdcef032d0c6d77e0dd
EBK21 <chkd13303@gmail.com>
2024-07-31 17:25:03 +0000

    update tc version and sync upstream

--- pre.sh
+++ pre.sh
@@ -36,7 +36,7 @@ if [ -e x86_64-linux-gnu ]; then
 echo "Hmm? Huh!"
 else
 if [ x$NO_TC_DOWN = x ] && ( [ ! -e /opt/newcc ] || [ ! -e ~/x86_64-linux-gnu ] ); then
-curl -L "https://github.com/eebssk1/aio_tc_build/releases/download/5688233d/x86_64-linux-gnu-native.tb2" | tar --bz -xf -
+curl -L "https://github.com/eebssk1/aio_tc_build/releases/download/ab3ea34e/x86_64-linux-gnu-native.tb2" | tar --bz -xf -
 mv x86_64-linux-gnu /opt/newcc
 fi
 fi
