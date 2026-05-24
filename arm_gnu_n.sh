b9411fe33abbb04688aeb4bd202e025acc1ceebf
EBK21 <chkd13303@gmail.com>
2026-05-24 09:36:43 +0000

    add arm64 native build

--- arm_gnu_n.sh
+++ arm_gnu_n.sh
@@ -11,22 +11,31 @@ if [ "x$(which ccache)" != "x" ]; then
 export CC="ccache gcc" CXX="ccache g++"
 fi
 
-export CFLAGS="-U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=1 -D__BUILD_NO_CON__ -flto-compression-level=1 -fprofile-partial-training -I/usr/local/include  @$CUR/gccflags -Wno-error=maybe-uninitialized"
+export CFLAGS="-U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=1 -D__BUILD_NO_CON__ -flto-compression-level=1 -fprofile-partial-training -I/usr/local/include  @$CUR/gccflagsa -Wno-error=maybe-uninitialized"
 export CXXFLAGS="$CFLAGS"
-export LDFLAGS="-L/usr/local/lib @$CUR/ldflags"
+export LDFLAGS="-L/usr/local/lib @$CUR/ldflagsa"
 
-export CFLAGS_FOR_TARGET="-D__BUILD_NO_CON__ -ffunction-sections -fdata-sections @$CUR/gccflags -Wno-error=maybe-uninitialized"
+export CFLAGS_FOR_TARGET="-D__BUILD_NO_CON__ -ffunction-sections -fdata-sections @$CUR/gccflagsa -Wno-error=maybe-uninitialized"
 export CXXFLAGS_FOR_TARGET="$CFLAGS_FOR_TARGET"
-export LDFLAGS_FOR_TARGET="@$CUR/ldflags"
+export LDFLAGS_FOR_TARGET="@$CUR/ldflagsa"
 
 export PATH=$CUR/out/bin:$PATH
+
+rm -r /opt/newcc
+
+curl -L "https://github.com/eebssk1/aio_tc_build/releases/latest/download/aarch64-linux-gnu-native.tb2" | tar --bz -xf -
+mv aarch64-linux-gnu /opt/newcc
+
+apt-get install -y libc6-dev-arm64-cross
+
+hash -r
 
 cd m_binutils; mkdir build; cd build
 
 echo current utc time 1 is $(date -u)
 TMS=$(date +%s)
 
-../configure --target=x86_64-linux-gnu --prefix=/usr --enable-pgo-build=lto --enable-nls --enable-plugins --enable-multilib --enable-compressed-debug-sections=all --enable-checking=release --enable-new-dtags --disable-gdb --disable-gdbserver --disable-sim --disable-gprof --disable-gprofng --with-system-zlib --with-zstd || exit 255
+../configure --target=aarch64-linux-gnu --prefix=/usr --enable-pgo-build=lto --enable-nls --enable-plugins --disable-multilib --enable-compressed-debug-sections=all --enable-checking=release --enable-new-dtags --disable-gdb --disable-gdbserver --disable-sim --disable-gprof --disable-gprofng --with-system-zlib --with-zstd || exit 255
 make -j$(($N+4)) all MAKEINFO=true || exit 255
 
 make -j install-strip DESTDIR=$CUR/tmp MAKEINFO=true
@@ -65,7 +74,7 @@ if [ x$NO_LTO = x ]; then
 LTO="--with-build-config=bootstrap-lto$DED"
 fi
 
-../configure --host=x86_64-linux-gnu --build=x86_64-linux-gnu --target=x86_64-linux-gnu --prefix=/usr --enable-version-specific-runtime-libs --enable-lto --disable-cet --enable-multiarch --with-arch-32=westmere --enable-multilib --with-multilib-list=m32,m64,mx32 --with-abi=m64 --disable-fixincludes --enable-libstdcxx-time --disable-libstdcxx-debug --disable-libstdcxx-pch --enable-graphite --enable-threads --enable-languages=c,c++,lto --with-linker-hash-style=gnu --enable-gnu-indirect-function --enable-gnu-unique-object --enable-plugin --enable-default-pie --with-gcc-major-version-only --enable-linker-build-id --with-default-libstdcxx-abi=new --enable-fully-dynamic-string --with-arch=broadwell --with-tune=icelake-client --enable-checking=release --without-included-gettext --enable-clocale=gnu $LTO --with-system-zlib --enable-shared=libgcc,libstdc++,libitm,libssp,libsanitizer --with-specs-file="$CUR/native.specs" || exit 255
+../configure --host=aarch64-linux-gnu --build=aarch64-linux-gnu --target=aarch64-linux-gnu --prefix=/usr --enable-version-specific-runtime-libs --enable-lto --disable-cet --disable-multiarch --disable-multilib --disable-fixincludes --enable-libstdcxx-time --disable-libstdcxx-debug --disable-libstdcxx-pch --enable-graphite --enable-threads --enable-languages=c,c++,lto --with-linker-hash-style=gnu --enable-gnu-indirect-function --enable-gnu-unique-object --enable-plugin --enable-default-pie --with-gcc-major-version-only --enable-linker-build-id --with-default-libstdcxx-abi=new --enable-fully-dynamic-string --with-arch=armv8.2-a --with-tune=cortex-a76.cortex-a55 --enable-checking=release --without-included-gettext --enable-clocale=gnu $LTO --with-system-zlib --enable-shared=libgcc,libstdc++,libitm,libssp,libsanitizer --with-specs-file="$CUR/arm.specs" || exit 255
 if [ "x$1" = "xprofile" ]; then
 make -j$(($N+4)) profiledbootstrap BOOT_CFLAGS="$CFLAGS" BOOT_CXXFLAGS="$CXXFLAGS" BOOT_LDFLAGS="$LDFLAGS" STAGE1_CFLAGS="$CFLAGS" STAGE1_CXXFLAGS="$CXXFLAGS" STAGE1_LDFLAGS="$LDFLAGS"  MAKEINFO=true || exit 255
 else
@@ -107,7 +116,7 @@ TMT1=$((($TME-$TMM)/60))
 TMA=$(($TMT0+$TMT1))
 echo "part 1 took $TMT0 min, part 2 took $TMT1 min, which sum to $TMA min together!"
 
-mv out x86_64-linux-gnu
-tar -I 'bzip2 -9' -cf x86_64-linux-gnu-native.tb2 x86_64-linux-gnu
-ln -s x86_64-linux-gnu out || exit 0
+mv out aarch64-linux-gnu
+tar -I 'bzip2 -9' -cf aarch64-linux-gnu-native.tb2 aarch64-linux-gnu
+ln -s aarch64-linux-gnu out || exit 0
 
