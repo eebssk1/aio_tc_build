854bb4e8e2857fecbe8091d308b239942956bc39
EBK21 <handleghincome@avalode.15360864.de>
2026-09-21 17:06:08 +0000

    try fix failed msys build

--- mingw64-ms2.sh
+++ mingw64-ms2.sh
@@ -20,9 +20,16 @@ cd mingw-w64-mingw-w64; mkdir build; cd build
 
 ../configure --without-headers --without-crt --with-tools=all --prefix=$CUR/out || exit 255
 make -j$(($N+3)) all MAKEINFO=true || exit 255
-make -j install-strip MAKEINFO=true
+make -j install-strip MAKEINFO=true || exit 255
 
 cd $CUR
+
+if [ "x$MS" != "x" ]; then
+SUF="_ms"
+fi
+
+mv mingw-crt/ucrt64${SUF}/bin/*.dll mingw-crt/ucrt64${SUF}/lib*/ || true
+cp -a mingw-crt/ucrt64${SUF}/. out/x86_64-w64-mingw32/ || exit 255
 
 cd m_binutils; mkdir build; cd build
 
@@ -31,28 +38,25 @@ export LDFLAGS="$LDFLAGS -L${PWD}/libiberty"
 ../configure --prefix=${MINGW_PREFIX} --target=x86_64-w64-mingw32 --enable-64-bit-bfd --disable-multilib --disable-shared --enable-nls --disable-rpath --with-libiconv-prefix=${MINGW_PREFIX} --with-sysroot=${MINGW_PREFIX} --enable-install-libiberty --enable-plugins --enable-deterministic-archives --disable-werror --enable-lto --with-system-zlib --with-zstd --disable-gdb --disable-gdbserver --disable-gprof --disable-gprofng || exit 255
 make -j$(($N+3)) all MAKEINFO=true || exit 255
 
-make -j install-strip prefix=$CUR/out MAKEINFO=true
+make -j install-strip prefix=$CUR/out MAKEINFO=true || exit 255
 TMM=$(date +%s)
 
 echo current utc time 2 is $(date -u)
 
 cd $CUR
 
-#not required under msys2?
-#tar --bzip -xf gcc-dep.tb2
-#cp -a gcc-dep/lib/*.a out/x86_64-w64-mingw32/lib/
-#cp -a gcc-dep/lib/*.dll out/bin/
-
-if [ "x$MS" != "x" ]; then
-SUF="_ms"
-fi
-
 cd m_gcc; mkdir build; cd build
 
-export MINGW_NATIVE_PREFIX=$(cygpath -am ${MINGW_PREFIX})
-
-sed -i "s#\\/mingw\\/#${MINGW_NATIVE_PREFIX//\//\\/}\\/#g" gcc/config/i386/mingw32.h
-sed -i "s#\\/ucrt64\\/#${MINGW_NATIVE_PREFIX//\//\\/}\\/#g" gcc/config/i386/mingw32.h
+cd ..
+for F in Makefile.in Makefile.tpl
+do
+grep -q -- '$(SYSROOT_CFLAGS_FOR_TARGET)' "$F" || exit 255
+grep -q -- 'GCC_FOR_TARGET=$(STAGE_CC_WRAPPER) @GCC_FOR_TARGET@' "$F" || exit 255
+grep -Fq -- '`if $(LEAN); then echo '\'' -isystem '\''; else echo '\'' -I'\''; fi`$$s/libstdc++-v3/libsupc++ \' "$F" || exit 255
+sed -i '/`if $(LEAN); then echo '\'' -isystem '\''; else echo '\'' -I'\''; fi`\$\$s\/libstdc++-v3\/libsupc++ \\/a\	  $(SYSROOT_CFLAGS_FOR_TARGET) \\' "$F" || exit 255
+sed -i 's#GCC_FOR_TARGET=$(STAGE_CC_WRAPPER) @GCC_FOR_TARGET@#GCC_FOR_TARGET=$(STAGE_CC_WRAPPER) @GCC_FOR_TARGET@ $(SYSROOT_CFLAGS_FOR_TARGET)#' "$F" || exit 255
+done
+cd build
 
 export lt_cv_deplibs_check_method='pass_all'
 export gcc_cv_have_tls=yes
@@ -64,15 +68,18 @@ export CXXFLAGS_FOR_TARGET="$CFLAGS_FOR_TARGET"
 
 echo current utc time 3 is $(date -u)
 
-../configure --prefix=$CUR/out --target=x86_64-w64-mingw32 --enable-bootstrap --with-build-config=bootstrap-O3 --enable-mingw-wildcard --enable-version-specific-runtime-libs --enable-checking=release --with-local-prefix=$CUR/out/x86_64-w64-mingw32/local --with-native-system-header-dir=${MINGW_PREFIX}/include --with-arch=haswell --with-tune=skylake --with-gcc-major-version-only --enable-tls --disable-cet --disable-vtable-verify --enable-plugin  --with-system-zlib --enable-libatomic --enable-threads=posix --enable-graphite --enable-fully-dynamic-string --enable-libstdcxx-filesystem-ts --enable-libstdcxx-time --disable-libstdcxx-pch --enable-libstdcxx-backtrace=yes --with-libstdcxx-zoneinfo="yes" --enable-lto --enable-libgomp --disable-libssp --disable-libvtv --enable-shared=libgcc,libstdc++,libgomp,libatomic --disable-multiarch --disable-multilib --disable-rpath --disable-nls --disable-werror --disable-symvers --disable-libstdcxx-debug --disable-win32-registry --enable-languages=c,c++,lto --disable-sjlj-exceptions --with-specs-file="$CUR/mingw64.specs" || exit 255
+../configure --prefix=$CUR/out --target=x86_64-w64-mingw32 --with-sysroot=$CUR/out/x86_64-w64-mingw32 --with-build-sysroot=$(cygpath -am $CUR/out/x86_64-w64-mingw32) --enable-bootstrap --with-build-config=bootstrap-O3 --enable-mingw-wildcard --enable-version-specific-runtime-libs --enable-checking=release --with-local-prefix=/local --with-native-system-header-dir=/include --with-arch=haswell --with-tune=skylake --with-gcc-major-version-only --enable-tls --disable-cet --disable-vtable-verify --enable-plugin  --with-system-zlib --enable-libatomic --enable-threads=posix --enable-graphite --enable-fully-dynamic-string --enable-libstdcxx-filesystem-ts --enable-libstdcxx-time --disable-libstdcxx-pch --enable-libstdcxx-backtrace=yes --with-libstdcxx-zoneinfo="yes" --enable-lto --enable-libgomp --disable-libssp --disable-libvtv --enable-shared=libgcc,libstdc++,libgomp,libatomic --disable-multiarch --disable-multilib --disable-rpath --disable-nls --disable-werror --disable-symvers --disable-libstdcxx-debug --disable-win32-registry --enable-languages=c,c++,lto --disable-sjlj-exceptions --with-specs-file="$CUR/mingw64.specs" || exit 255
+export CPATH="$CUR/out/x86_64-w64-mingw32/include${CPATH:+:$CPATH}"
+export LIBRARY_PATH="$CUR/out/x86_64-w64-mingw32/lib${LIBRARY_PATH:+:$LIBRARY_PATH}"
+export MSYS2_ARG_CONV_EXCL="-D"
 make -j$(($N+3)) bootstrap STAGE1_CFLAGS="-g1 -Os" MAKEINFO=true && make -j$(($N+3)) all MAKEINFO=true
 
 if [ "$?" != "0" ]; then
 echo "Error ed !"
-exit 0
+exit 255
 fi
 
-make -j install-strip MAKEINFO=true
+make -j install-strip MAKEINFO=true || exit 255
 
 GCV=$(cat ../gcc/BASE-VER | cut -d'.' -f 1)
 
@@ -90,9 +97,6 @@ ln -s ${TB} ${TB}-${GCV}
 fi
 done
 
-mv mingw-crt/ucrt64${SUF}/bin/*.dll mingw-crt/ucrt64${SUF}/lib*/ || true
-cp -a mingw-crt/ucrt64${SUF}/. out/x86_64-w64-mingw32/
-
 echo current utc time 4 is $(date -u)
 TME=$(date +%s)
 TMT0=$((($TMM-$TMS)/60))
@@ -102,8 +106,7 @@ echo "part 1 took $TMT0 min, part 2 took $TMT1 min, which sum to $TMA min togeth
 
 cd $CUR
 
-mv out x86_64-w64-mingw32-msys2$SUF
-tar -I 'bzip2 -9' -cf x86_64-w64-mingw32-cross_msys2$SUF.tb2 x86_64-w64-mingw32-msys2$SUF
+mv out x86_64-w64-mingw32-msys2$SUF || exit 255
+tar -I 'bzip2 -9' -cf x86_64-w64-mingw32-cross_msys2$SUF.tb2 x86_64-w64-mingw32-msys2$SUF || exit 255
 
 exit 0
-
